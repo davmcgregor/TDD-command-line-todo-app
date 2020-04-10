c64cbb873d4d4ffd07e697e11901b1c7fe9e86ad
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-10 16:51:19 +0100

    Refactor to remove user interface from logic

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,13 +4,9 @@ class Todo
   def initialize
     @list = []
     @done_list = []
-    print_list
-    menu
   end
   
   def print_list
-    puts "\nHere is your Todo list:"
-    puts "-" * 37
     if @list.length < 1
       puts "empty"
     else
@@ -21,8 +17,6 @@ class Todo
   end
 
   def print_done_list
-    puts "\nHere is your Completed list:"
-    puts "-" * 37
     if @done_list.length < 1
       puts "empty"
     else
@@ -32,53 +26,49 @@ class Todo
     end
   end
 
-  def menu
+  def add(new_todo)
+    @list << new_todo
+  end
+
+  def edit(todo_num, todo_edit)
+    @list[todo_num - 1] = todo_edit
+  end
+
+  def done(done_num)
+    @done_list << @list[done_num - 1]
+    @list.delete_at(done_num - 1)
+  end
+
+  def start
+  loop do
+    puts "\nHere is your Todo list:"
+    puts "-" * 37
+    print_list
+    puts "\nHere is your Completed list:"
+    puts "-" * 37
+    print_done_list
     puts "\nType ADD, EDIT, DONE or exit: "
     user_input = STDIN.gets.chomp
     case user_input
       when "ADD"
-        add
+        puts "Please enter new todo: "
+        new_todo = STDIN.gets.chomp
+        add(new_todo)
       when "EDIT"
-        edit
+        puts "Please enter the number of the todo you wish to edit: "
+        todo_num = STDIN.gets.chomp.to_i
+        puts @list[todo_num - 1]
+        puts "Please enter new todo text: "
+        todo_edit = STDIN.gets.chomp
+        edit(todo_num, todo_edit)
       when "DONE"
-        done
+        puts "Please enter the number of the todo you have done: "
+        done_num = STDIN.gets.chomp.to_i
+        done(done_num)
       when "exit"
-        exit
-      else
-        print_list
-        print_done_list
-        menu
+        break
+      end
     end
-  end
-
-  def add
-    puts "Please enter new todo: "
-    new_todo = STDIN.gets.chomp
-    @list << new_todo
-    print_list
-    print_done_list
-    menu
-  end
-
-  def edit
-    puts "Please enter the number of the todo you wish to edit: "
-    todo_num = STDIN.gets.chomp.to_i
-    puts @list[todo_num - 1]
-    puts "Please enter new todo text: "
-    @list[todo_num - 1] = STDIN.gets.chomp
-    print_list
-    print_done_list
-    menu
-  end
-
-  def done
-    puts "Please enter the number of the todo you have completed: "
-    done_num = STDIN.gets.chomp.to_i
-    @donelist << @list[done_num - 1]
-    @list.delete_at(done_num - 1)
-    print_list
-    print_done_list
-    menu
   end
 
 end
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -9,42 +9,65 @@ describe Todo do
     it 'initializes with an empty done list' do
       expect(subject.done_list).to eq([])
     end
+  end
 
-    it 'prints the todo list to the console' do
-      expect do
-        myTodo = Todo.new
-      end.to output("").to_stdout
+  describe '#print_list' do
+    it 'prints an empty todo list' do
+      expect { subject.print_list }.to output("\nHere is your Todo list:\n-------------------------------------\nempty\n").to_stdout
+    end
+
+    it 'prints the todo list with an index' do
+      subject.list << "This is test todo number 1"
+      expect { subject.print_list }.to output("\nHere is your Todo list:\n-------------------------------------\n1. This is test todo number 1\n").to_stdout
     end
   end
-  
+
+  describe '#print_done_list' do
+    it 'prints an empty done list' do
+      expect { subject.print_done_list }.to output("\nHere is your Completed list:\n-------------------------------------\nempty\n").to_stdout
+    end
+
+    it 'prints the done list with an index' do
+      subject.done_list << "This is test done number 1"
+      expect { subject.print_done_list }.to output("\nHere is your Completed list:\n-------------------------------------\n1. This is test done number 1\n").to_stdout
+    end
+  end
+
   describe '#add' do
-    it 'Prints a todo item to the console' do
-      expect do
-        subject.add("this is a test todo")
-      end.to output("1. this is a test todo\n").to_stdout
-    end
-
-    it 'adds a todo item to the todo list' do
-      subject.add("this is a test todo")
-      expect(subject.list).to include("this is a test todo")
+    it 'allows a user to ADD a todo' do
+      allow(subject).to receive(:gets).and_return("ADD\n", "This is todo 1\n", "exit\n")
+      subject.add
+      expect(subject.list).to eq(["This is todo 1\n"])
     end
   end
 
-  describe '#print' do
-    it 'Prints the entire list of todos' do
-      subject.add("this is the first todo")
-      subject.add("this is the second todo")
-      expect do
-        subject.print
-      end.to output("1. this is the first todo\n2. this is the second todo\n").to_stdout
-    end
-  end
+  #   it 'adds a todo item' do
+  #     displayer.stub(:gets).and_return("ADD\n")
+  #     expect(displayer.meny)
+  #     expect do
+  #       subject.menu
+  #     end.to output("Please enter new todo: \n").to_stdout
+  #   end
+  
+  #   # it 'Prints out a selection of commands' do
+  #   #   expect do
+  #   #     subject.menu
+  #   #   end.to output("\nType ADD, EDIT, DONE or exit: ").to_stdout
+  #   # end
 
-  describe '#complete' do
-    it 'allows incomplete todos to be edited' do
-      subject.add("this is the first t...")
-      subject.complete(1, "this is the first todo")
-      expect(subject.list[0]).to eq("this is the first todo")
-    end
-  end
+  # end
+
+  # describe '#add' do
+  #   it 'Prints a todo item to the console' do
+  #     expect do
+  #       subject.add("this is a test todo")
+  #     end.to output("1. this is a test todo\n").to_stdout
+  #   end
+
+  #   it 'adds a todo item to the todo list' do
+  #     subject.add("this is a test todo")
+  #     expect(subject.list).to include("this is a test todo")
+  #   end
+  # end
+
 end
